349badc2a655a7a6d290695b0c1a1b9b0af4d858
Ruiner <jalenowen9@gmail.com>
2021-11-04 18:28:23 -0500

    Add Engineer Task Progression

--- source/Patches/CrewmateRoles/EngineerMod/HUDClose.cs
+++ source/Patches/CrewmateRoles/EngineerMod/HUDClose.cs
@@ -19,7 +19,15 @@ namespace TownOfUs.CrewmateRoles.EngineerMod
             foreach (var role in Role.GetRoles(RoleEnum.Engineer))
             {
                 var engineer = (Engineer) role;
-                if (CustomGameOptions.EngineerFixPer == EngineerFixPer.Round) engineer.UsedThisRound = false;
+                if (CustomGameOptions.RoleProgressionOn) {
+                    if (engineer.RemainingRounds > 0 && engineer.UsedThisRound)
+                    {
+                        engineer.UsedThisRound = false;
+                        engineer.TotalUses++;
+                    }
+                } else {
+                    if (CustomGameOptions.EngineerFixPer == EngineerFixPer.Round) engineer.UsedThisRound = false;
+                }
             }
         }
     }
--- source/Patches/Roles/Engineer.cs
+++ source/Patches/Roles/Engineer.cs
@@ -11,8 +11,35 @@ namespace TownOfUs.Roles
             TaskText = () => "Vent and fix a sabotage from anywhere!";
             Color = Patches.Colors.Engineer;
             RoleType = RoleEnum.Engineer;
+            if (CustomGameOptions.RoleProgressionOn) UsedThisRound = true;
         }
 
         public bool UsedThisRound { get; set; } = false;
+
+        public int TotalUses { get; set; } = 0;
+
+        public int RemainingRounds {
+            get {
+                int Total = 0;
+                if (Tier1) Total++;
+                if (Tier2) Total++;
+                if (Tier3) Total++;
+                if (Tier4) Total++;
+                return Total - TotalUses;
+            }
+        }
+
+        protected override void OnTierUp()
+        {
+            if (CustomGameOptions.RoleProgressionOn)
+            {
+                if (RemainingRounds > 0 && UsedThisRound)
+                {
+                    UsedThisRound = false;
+                    TotalUses++;
+                }
+            }
+        }
+
     }
 }
